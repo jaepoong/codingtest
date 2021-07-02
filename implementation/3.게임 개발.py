a0a86356d3f22d06fc0086d8a23151cccd57dd3c
jaepoong <62787572+jaepoong@users.noreply.github.com>
2021-07-02 13:18:57 +0900

    ch4
    BFS기초까지

--- implementation/3.게임 개발.py
+++ implementation/3.게임 개발.py
@@ -1,9 +1,39 @@
-row,column=map(int,input().split())
-(x,y,f_d)=map(int,input().split())
-x=[]
-for _ in range(row):
-    x.append(list(map(int,input().split())))
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-direct=[0,1,2,3]
+n,m=map(int,input().split())
+d=[[0]*m for _ in range(n)]# 방문한 위치
+(x,y,direction)=map(int,input().split())
+a=[]
+for _ in range(n):
+    a.append(list(map(int,input().split()))) # 맵 지형
+d[x][y]=1
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
+def turn_left():
+    global direction
+    direction -=1
+    if direction==-1:
+        direction=3
+count=1
+turn_count=0
 while True:
+    turn_left()
+    nx=x+dx[direction]
+    ny=y+dy[direction]
+    if (d[x][y]==0 and a[x][y]==0):
+        d[nx][ny]=1
+        x=nx
+        y=ny
+        count+=1
+        turn_time=0
+        continue
+    else:
+        turn_count+=1
+    if turn_count==4:
+        nx=x-dx[direction]
+        ny=y-dy[direction]
+        if a[nx][ny]==0:
+            x=nx
+            y=ny
+        else:
+            break
+        turn_count=0
+print(count)
