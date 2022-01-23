5c1c15a60c05b5cba537cc03ffd78a202b5da66e
설재중 <tjfwownd@naver.com>
2022-01-23 20:19:15 +0900

    r

---
+++ 이취코/greedy/만들수없는금액.py
@@ -0,0 +1,4 @@
+n=int(input())
+li=list(map(int,input().split()))
+de=[False]*(n+1)
+li.sort()
--- 이취코/greedy/모험가길드.py
+++ 이취코/greedy/모험가길드.py
@@ -1,12 +1,12 @@
 n=int(input())
 li=list(map(int,input().split()))
-li.sort(reverse=True)
-x=li[0]
+li.sort()
+count=0
 result=0
 for i in range(len(li)):
-    x-=1
-    if x==0:
-        x=li[i]
+    count+=1
+    if count>=i:
         result+=1
+        count=0
 
 print(result)
